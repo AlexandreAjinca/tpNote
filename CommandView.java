dfb92da912072e271df346b5d7c9bd7cbd226869
Alexandre Ajinça <a.ajinca@laposte.net>
2018-12-10 18:13:15 +0100

    Add files via upload

--- CommandView.java
+++ CommandView.java
@@ -1,6 +1,9 @@
 package view;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.*;
 
@@ -17,7 +20,11 @@ public class CommandView extends JFrame{
 	private JComboBox<String> client;
 	private JButton commander;
 	
-	private Commandes command;
+	private BDD bd;
+	private int idCommand;
+	private ArrayList<String> nomProduits;
+	private ArrayList<Integer> quantités;
+	private int idClient;
 	
 	public static void main(String[] args) {  
         CommandView a = new CommandView();  
@@ -34,6 +41,11 @@ public class CommandView extends JFrame{
         GridBagLayout layout = new GridBagLayout();
         this.setLayout(layout);
         
+        bd = new BDD();
+                
+        nomProduits = new ArrayList<String>();
+        quantités = new ArrayList<Integer>();
+        
         gbc.fill=GridBagConstraints.HORIZONTAL;
         gbc.gridx=0;
         gbc.gridy=0;
@@ -43,24 +55,37 @@ public class CommandView extends JFrame{
         for(int i= 0;i<prods.getNomProduits().size();i++) {
         	fleur.addItem(prods.getNomProduits().get(i));
         }
-        this.add(fleur);
+        this.add(fleur,gbc);
         
         gbc.gridx = 2;
         gbc.gridy=0;
         gbc.gridwidth=1;
         nombre = new JTextField();
-        this.add(nombre);
+        this.add(nombre,gbc);
         
         gbc.gridx=3;
         gbc.gridy=0;
         gbc.gridwidth = GridBagConstraints.REMAINDER;
         add = new JButton("ajouter");
         
+        class AjouterProduitListener implements ActionListener {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==add) {
+					nomProduits.add((String) fleur.getSelectedItem());
+					quantités.add(Integer.parseInt(nombre.getText()));
+					produits.setText(produits.getText() + "\n " + fleur.getSelectedItem() + " " + Integer.parseInt(nombre.getText()));
+				}
+			}
+        }
+        
+        add.addActionListener(new AjouterProduitListener());
+        this.add(add,gbc);
+        
         gbc.gridx = 0;
         gbc.gridy=1;
         gbc.gridheight=fleur.getHeight();
         produits = new JTextField();
-        this.add(produits);
+        this.add(produits,gbc);
         
         gbc.gridx = 0;
         gbc.gridy = 6;
@@ -70,12 +95,30 @@ public class CommandView extends JFrame{
         for(int i = 0; i< clts.getNomClient().size();i++) {
         	client.addItem(clts.getNomClient().get(i));
         }
-        this.add(client);
+        this.add(client,gbc);
         
         gbc.gridx = 2;
         gbc.gridy = 6;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
         commander = new JButton("Commander!");
-        this.add(commander);
+        
+        class CreerCommandeListener implements ActionListener {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==commander) {
+					idClient = client.getSelectedIndex();
+					Clients cl = new Clients();
+					bd.addCommandtoBDD(cl.getIdClient().get(idClient), nomProduits, quantités);
+				}
+			}
+        }
+        commander.addActionListener(new CreerCommandeListener());
+   
+        this.add(commander,gbc);
+        
+        Commandes cmd = BDD.getCmds();
+        idCommand = cmd.getIdCommande().size()+1;
+        
+        
         
         this.setVisible(true);
 	}
